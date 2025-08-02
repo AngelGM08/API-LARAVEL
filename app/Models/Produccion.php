dd33a1bd1146a251791b601ddebac48dc9a77e28
AnaSel17 <anasel17@gmail.com>
2025-08-02 03:49:50 -0600

    Update Produccion.php
    Se realizo el corrección de id_platillo a id_tamal

--- app/Models/Produccion.php
+++ app/Models/Produccion.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tamal;
 
 class Produccion extends Model
 {
     protected $table = 'produccion';
     protected $primaryKey = 'id_produccion';
+
     protected $fillable = [
-        'id_platillo',
+        'id_tamal',
         'fecha',
         'cantidad_total',
     ];
+
+    public function tamal()
+    {
+         return $this->belongsTo(Tamal::class, 'id_tamal');
+    }
 }
