2ff00c1a6a7e82a15a259eea9efa4c9b2ba06283
AnaSel17 <anasel17@gmail.com>
2025-08-02 03:50:04 -0600

    Update Tamal.php
    Se pasa el campo id_tamal a Produccion

--- app/Models/Tamal.php
+++ app/Models/Tamal.php
@@ -3,13 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Produccion;
 
 class Tamal extends Model
 {
     protected $table = 'tamal';
     protected $primaryKey = 'id_tamal';
-    protected $fillable = [
-        'nombre_tamal',
-        'descripcion'
-    ];
+
+    protected $fillable = ['nombre_tamal'];
+
+    public function producciones()
+    {
+        return $this->hasMany(Produccion::class, 'id_tamal');
+    }
 }
